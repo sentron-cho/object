be89d1fc8c3657a0e8eaa14004ffe14ed1df61f1
sentron-cho <sentron@daum.net>
2020-03-14 21:22:58 +0900

    colorbox 스토리 및 테스트 추가

--- src/Colorbox.js
+++ src/Colorbox.js
@@ -2,26 +2,120 @@ import React from 'react';
 import cx from 'classnames/bind';
 import styled from 'styled-components';
 import { CompactPicker } from 'react-color';
-import { Svg } from './index'
+import { Svg, cs } from './index'
 
 const StyledObject = styled.div` {
   &.color-box {
-    .picker-box { width: 100% !important; }
-    .label { display: inline-block; padding: 0; font-size: 12px; text-align: left; border: 0; font-weight: 500;}
-    input { background: #eaeaea !important; }
+    ${cs.max.width(400)} ${cs.pos.relative} 
 
-    .btn-clear { float: right; right: 15px; bottom: 15px; position: absolute; }
-
-    &.white {
-      & > div > div:first-child { background: #ffffff !important; border: 1px solid #8e8e8e !important; }
+    & > div > div:first-child { ${cs.over.hidden} }
+    
+    &.label {
+      & > div { ${cs.p.t20} }
     }
 
-    &.black {
-      & > div > div:first-child { background: #212121 !important; border: 1px solid #8c8c8cc9 !important; }
+    .flexbox-fix > div { ${cs.bg.get('transparent !important')} }
+
+    .picker-box { 
+      ${cs.w.get('100% !important')}
+      & > div { ${cs.w.calc('100% - 10px')} } 
+    }
+
+    .label { 
+      ${cs.disp.inblock} ${cs.p.a0} ${cs.font.sm} ${cs.align.left} 
+      ${cs.border.none} ${cs.font.bold} ${cs.z.front} ${cs.p.l5} ${cs.p.t3}
+      ${cs.w.calc('100% - 10px')}
+    }
+
+    input { ${cs.bg.get(`${cs.color.trans} !important;`)} }
+
+    .btn-clear { ${cs.align.lbottom} ${cs.bottom(7)} ${cs.left(8)} .svg-path { ${cs.fill.dark} } }
+
+    // &.white {
+    //   & > div > div:first-child { background: #ffffff !important; border: 1px solid #8e8e8e !important; }
+    // }
+
+    // &.black {
+    //   & > div > div:first-child { background: #212121 !important; border: 1px solid #8c8c8cc9 !important; }
+    // }
+
+    
+    &.trans { & > div > div:first-child { ${cs.bg.get(`${cs.color.white} !important;`)} } }
+    &.sky { & > div > div:first-child { ${cs.bg.get(`${cs.color.sky} !important;`)} } }
+    &.orange { & > div > div:first-child { ${cs.bg.get(`${cs.color.orange} !important;`)} } 
+      .label { ${cs.font.white} } 
+      input { ${cs.font.color(`${cs.color.white} !important`)} ${cs.bg.get(`${cs.color.orange} !important;`)}
+        & + span { ${cs.font.color(`${cs.color.lightgray} !important`)} } 
+      }
+      .btn-clear { .svg-path { ${cs.fill.white} } }
+    }
+    &.green { & > div > div:first-child { ${cs.bg.get(`${cs.color.green} !important;`)} } 
+      .label { ${cs.font.white} } 
+      input { ${cs.font.color(`${cs.color.white} !important`)} ${cs.bg.get(`${cs.color.green} !important;`)}
+        & + span { ${cs.font.color(`${cs.color.lightgray} !important`)} } 
+      }
+      .btn-clear { .svg-path { ${cs.fill.white} } }
+    }
+    &.red { & > div > div:first-child { ${cs.bg.get(`${cs.color.red} !important;`)} }
+      .label { ${cs.font.white} } 
+      input { ${cs.font.color(`${cs.color.white} !important`)} ${cs.bg.get(`${cs.color.red} !important;`)}
+        & + span { ${cs.font.color(`${cs.color.lightgray} !important`)} } 
+      }
+      .btn-clear { .svg-path { ${cs.fill.white} } }
+    }
+    &.primary { 
+      & > div > div:first-child { ${cs.bg.get(`${cs.color.primary} !important;`)} } 
+      .label { ${cs.font.white} } 
+      input { ${cs.font.color(`${cs.color.white} !important`)} ${cs.bg.get(`${cs.color.primary} !important;`)}
+        & + span { ${cs.font.color(`${cs.color.lightgray} !important`)} } 
+      } 
+      .btn-clear { .svg-path { ${cs.fill.white} } }
+    }
+    &.gray { & > div > div:first-child { ${cs.bg.get(`${cs.color.gray} !important;`)} } 
+      input { ${cs.font.color(`${cs.color.white} !important`)} ${cs.bg.get(`${cs.color.gray} !important;`)}
+        & + span { ${cs.font.color(`${cs.color.lightgray} !important`)} } 
+      }
+      .btn-clear { .svg-path { ${cs.fill.white} } }
+    }
+    &.dark { 
+      & > div > div:first-child { ${cs.bg.get(`${cs.color.dark} !important;`)} } 
+      .label { ${cs.font.white} } 
+      input { ${cs.font.color(`${cs.color.white} !important`)} ${cs.bg.get(`${cs.color.dark} !important;`)}
+        & + span { ${cs.font.color(`${cs.color.lightgray} !important`)} } 
+      } 
+      .btn-clear { .svg-path { ${cs.fill.lightgray} } }
+    }
+    &.black { 
+      & > div > div:first-child { ${cs.bg.get(`${cs.color.black} !important;`)} } 
+      .label { ${cs.font.white} } 
+      input { ${cs.font.color(`${cs.color.white} !important`)} ${cs.bg.get(`${cs.color.black} !important;`)} 
+        & + span { ${cs.font.color(`${cs.color.lightgray} !important`)} } 
+      } 
+      .btn-clear { .svg-path { ${cs.fill.lightgray} } }
     }
     
     &.no-shadow {
       & > div > div:first-child { border: 1px solid #eaeaea; box-shadow: none; box-shadow: none !important; }
+    }
+
+    &.border {
+      & > div > div:first-child { ${cs.box.line} ${cs.border.radius('0 !important')} }
+    }
+
+    &.radius {
+      & > div > div:first-child { ${cs.border.radius('5px !important')} }
+    }
+
+    .label {
+      ${({title}) => title && title.align && cs.font.align(title.align)}
+      ${({title}) => title && title.color && cs.font.color(title.color)}
+    }
+
+    & > div > div:first-child {
+      ${({border}) => border && cs.box.line}
+      ${({border}) => border && border.color && cs.border.color(border.color)}
+      ${({border}) => border && border.radius && cs.border.radius(`${border.radius} !important`)}
+      ${({border}) => border && border.width && cs.border.width(border.width)}
     }
 
     @media screen and (max-width : 860px) {
@@ -83,13 +177,14 @@ export default class Colorbox extends React.PureComponent {
   render() {
     const { props, state } = this;
     const type = props.type ? props.type : 'compact';
-    const { theme, clear } = props;
+    const { theme, clear, label, className } = props;
+    const { border, title } = props.options || {border: null, title: null};
 
     return (
-      <StyledObject className={cx('color-box', this.props.className, { theme })}>
-        {props.label && <label className="label">{props.label}</label>}
+      <StyledObject className={cx('color-box', className, { theme }, {label} )} border={border} title={title} >
+        {label && <label className="label">{label}</label>}
         {type === "compact" && <CompactPicker className={"picker-box"} onChange={this.onChanged} color={state.value} />}
-        {clear && <Svg className="btn-clear sm" onClick={this.onClear} eid={"clear"} icon={'clear'} />}
+        {clear && <Svg className="btn-clear xs" onClick={this.onClear} eid={"clear"} icon={'clear'} />}
       </StyledObject >
     )
   }
---
+++ stories/22-Colorbox.stories.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { optionsKnob as options, withKnobs, text, boolean, radios, number, button } from '@storybook/addon-knobs';
+import styled from 'styled-components';
+import cx from 'classnames/bind'
+import { Linebox } from './00-Frame';
+import { cs, Colorbox, Util } from '../src';
+
+const StyledObject = styled.span`{
+  &.t-main {
+    .lb-box { ${cs.w.get(800)} ${cs.p.b30} }
+
+    .lb-box .lb-li > * { ${cs.m.b10} }
+
+    .res-view { 
+      ${cs.h.get(100)} ${cs.w.get(800)} ${cs.bg.lightgray} 
+      p { ${cs.m.a5} }
+    }    
+  }
+}`;
+
+export default { title: 'object|Colorbox', component: Colorbox, decorators: [withKnobs] };
+
+const samplecode = (value, classname = '') => `<Colorbox className={"${classname}"} ${value} />`;
+
+const list = [
+  { eid: 'card', title: 'card', icon: 'thead' },
+  { eid: 'list', title: 'list', icon: 'list' },
+  { eid: 'menu', title: 'menu', icon: 'menu' },
+  { eid: 'user', title: 'user', icon: 'user' },
+]
+
+export const object = () => {
+  const size = options('size',
+    { 'none': '', 'xs(x small)': 'xs', 'sm(small)': 'sm', 'md(middle)': 'md', 'lg(large)': 'lg', 'xl(x large)': 'xl' },
+    '', { display: 'inline-radio' }, 'Other');
+  const bg = options('background',
+    { none: '', primary: 'primary', orange: 'orange', green: 'green', red: 'red', gray: 'gray', dark: 'dark', black: 'black' },
+    '', { display: 'inline-radio' }, 'Other');
+
+  const istitle = boolean('title options', false);
+  const titlecolor = text('title color', '#353535');
+  const titlealign = options('title align', { 'left': 'left', 'center': 'center', 'right': 'right' },
+    '', { display: 'inline-radio' }, 'Other');
+
+  const isborder = boolean('border options', false);
+  const border = isborder ? text('border color', '#909090') : '';
+  const radius = isborder ? text('border radius', '1px') : '';
+  const width = isborder ? text('border width', '1px') : '';
+
+  const [result, setResult] = useState(null);
+
+  const onChange = (hex, rgb, e) => {
+    setResult(`onChange(hex = ${hex}, rgb = ${rgb}, e)`);
+  }
+
+  const opt = {
+    border: isborder ? { radius: radius, color: border, width: width } : null,
+    title: istitle ? { align: titlealign, color: titlecolor } : null,
+  };
+
+  return (
+    <StyledObject className={"t-main"}>
+      <Linebox title={"toggle button"} className={"nomargin"} desc={"Knobs 옵션을 통해 미리보기가 가능합니다."} sample={samplecode("", "")} box={false}>
+        <Colorbox className={cx(size, bg)} options={opt} label={'colorbox'} clear={true} onChange={onChange} />
+      </Linebox>
+
+      <div className={"res-view"}>
+        <p>onClick</p>
+        <p>{result}</p>
+      </div>
+    </StyledObject>
+  );
+};
+
+object.story = { name: 'Base' };
+
+
+export const color = () => {
+  return (
+    <StyledObject className={"t-main"} id={"f0001"}>
+      <Linebox title={"color"} sample={samplecode('', 'sky')}>
+        <Colorbox clear={true} className={"trans"} label={"trans"} />
+        <Colorbox clear={true} className={"sky"} label={"sky"} />
+        <Colorbox clear={true} className={"orange"} label={"orange"} />
+        <Colorbox clear={true} className={"green"} label={"green"} />
+        <Colorbox clear={true} className={"red"} label={"red"} />
+        <Colorbox clear={true} className={"primary"} label={"primary"} />
+        <Colorbox clear={true} className={"gray"} label={"gray"} />
+        <Colorbox clear={true} className={"dark"} label={"dark"} />
+        <Colorbox clear={true} className={"black"} label={"black"} />
+      </Linebox>
+    </StyledObject>
+  );
+};
+
+export const title = () => {
+  return (
+    <StyledObject className={"t-main"} id={"f0001"}>
+      <Linebox title={"title options"} sample={samplecode('', 'sky')}>
+        <Colorbox label={"trans"} options={{ title: { align: "left", color: "red" } }} />
+        <Colorbox label={"trans"} options={{ title: { align: "center", color: "blue" } }} />
+        <Colorbox label={"trans"} options={{ title: { align: "right", color: "black" } }} />
+      </Linebox>
+    </StyledObject>
+  );
+};
+
+export const border = () => {
+  return (
+    <StyledObject className={"t-main"} id={"f0001"}>
+      <Linebox title={"border options"} sample={samplecode('', 'sky')}>
+        <Colorbox label={"trans"} options={{ border: { radius: '5px', color: "red", width: '1px' } }} />
+        <Colorbox label={"trans"} options={{ border: { radius: '10px', color: "blue", width: '2px' } }} />
+        <Colorbox label={"trans"}
+          options={{ border: { radius: '20px', color: "black", width: '3px' }, title: { align: "center" } }}
+        />
+      </Linebox>
+    </StyledObject>
+  );
+};

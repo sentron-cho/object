80237ec51f8ae65e679f42edd153b8fa10afd00e
sentron-cho <sentron@hanmail.net>
2020-03-17 14:23:57 +0900

    mediabox 스토리 및 테스트 추가

--- src/Mediabox.js
+++ src/Mediabox.js
@@ -4,20 +4,27 @@ import cx from 'classnames/bind';
 import styled from 'styled-components';
 import ReactPlayer from 'react-player';
 import YouTube from 'react-player/lib/players/YouTube';
-import { Util, Svg } from './index';
-import { EID, CONT_TYPE } from './Config'
+import { Util, Svg, cs } from './index';
+import { EID } from './Config'
 import { IMG } from './Icons';
 
-const StyledObject = styled.div`{
-  &.media-box { position: relative; width: 100%; height: ${(props) => props.height ? props.height : "fit-content" };
-    .cont-frame {
-      position: relative; border: 2px solid rgba(255, 255, 255, 0); border-radius: 5px; height: 100%;
-      width: 100%; max-height: 562px; object-fit: cover; object-position: center center; transition: opacity 500ms ease 0s; 
+export const CONT_TYPE = {
+  IMAGE: "image",
+  VIDEO: "video",
+  YOUTUBE: "youtube",
+  TEXT: "text",
+  PDF: "pdf",
+  LINK: "link",
+};
 
-      // &.pdf { padding: 10% 20%; background: rgba(255, 255, 255, 0.5); }
-      // &.pdf-view { float: left; position: absolute; top: 88%; text-align: center;
-      //   display: inline; width: 100%; font-size: 2vw; color: #ffa212; font-weight: 600;
-      // }
+const StyledObject = styled.div`{
+  &.media-box { 
+    ${cs.pos.relative} ${cs.w.full} ${cs.noselect} ${cs.box.inner} ${cs.over.hidden}
+    ${({ height }) => cs.h.get(height || "fit-content")};
+
+    .cont-frame {
+      position: relative; height: 100%;
+      width: 100%; max-height: 562px; object-fit: cover; object-position: center center; transition: opacity 500ms ease 0s; 
       
       &.youtube, &.video { width: 100% !important; height: 100% !important; }
     }
@@ -42,6 +49,25 @@ const StyledObject = styled.div`{
     &.sm { .image { max-height: 450px; } }
 
     .noimage { border: 1px solid rgba(255, 255, 255, 0.1); }
+
+    
+    &.border { ${cs.box.line} ${cs.border.lightgray} }
+    &.radius { ${cs.box.radius} }
+    &.right { ${cs.align.right} ${cs.pos.relative} }
+    &.show-loading { .loading-box { ${cs.opac.show} } }
+
+    &.left { }
+    &.right { ${cs.align.right} ${cs.top(0)} }
+    &.center { ${cs.align.xcenter} ${cs.top(0)} }
+    &.top { ${cs.align.top} }
+    &.middle { ${cs.align.ycenter} }
+    &.bottom { ${cs.align.bottom} }
+    &.center.middle { ${cs.pos.absolute} ${cs.top("50%")} ${cs.left("50%")} ${cs.align.get("translate(-50%, -50%)")} }
+
+    ${({ border }) => border && cs.box.line}
+    ${({ border }) => border && border.color && cs.border.color(border.color)}
+    ${({ border }) => border && border.radius && cs.border.radius(border.radius)}
+    ${({ border }) => border && border.width && cs.border.width(border.width)}
 
     @media screen and (max-width : 1280px) {
     }
@@ -97,7 +123,7 @@ export default class Mediabox extends React.PureComponent {
   }
 
   getHeight = () => {
-    let { rate = "4:3", size } = this.props;
+    let { rate = "4:3", size, maxwidth } = this.props;
     if (size != null) {
       switch (size) {
         case 'full': return "100%";
@@ -114,8 +140,11 @@ export default class Mediabox extends React.PureComponent {
     const temps = rate.split(":");
     const x = temps[0];
     const y = temps[1];
-    const width = this.box.offsetWidth;
-    const height = width * (y / x);
+    let width = this.box.offsetWidth;
+    if (maxwidth && parseInt(width) > parseInt(maxwidth)) {
+      width = parseInt(maxwidth);
+    }
+    const height = Math.floor(width * (y / x));
     return `${height}px`;
   }
 
@@ -125,47 +154,38 @@ export default class Mediabox extends React.PureComponent {
 
   render() {
     const { props, state } = this;
-    const { type = "image", playing = true, controls = true } = props;
+    const { error } = state;
+    const { type = "image", playing = true, controls = true, fit = "contain", edited } = props;
     const pointer = !Util.isEmpty(props.link) ? 'pointer' : '';
+    const src = props.src || props.url;
+    const noimage = !src || error;
+    const isguide = (edited && noimage);
+    const { border } = props.options || { border: null };
+
     const renderContents = () => {
-      const { error } = state;
-
-      // const url = (props.url != null && props.url.charAt(0) != '/' ? `/${props.url}` : props.url);
-      // const urldata = props.url.indexOf("data:") === 0 ? props.url : props.url;
-      // const styled = {};
-      // const opts = {
-      //   height: '390', width: '640',
-      //   playerVars: { autoplay: 1 }
-      // }
-      // const url = (type === CONT_TYPE.YOUTUBE) ? props.url.replace("https://youtu.be/", "") : props.url;
-
-      if (error || !props.url) {
-        return <img alt="img" className={cx('cont-frame', type, 'noimage')} src={IMG.NoimageBig} onLoad={this.onLoad}/>
+      if (error || !src) {
+        return <img alt="img" className={cx('cont-frame', type, 'noimage')} src={IMG.NoimageBig} onLoad={this.onLoad} />
       } else {
         return (
           <React.Fragment>
             {type === CONT_TYPE.IMAGE && <img alt="img" className={cx("cont-frame", type, pointer)}
-              src={props.url} onLoad={this.onLoad} onError={this.onError} />}
+              src={src} onLoad={this.onLoad} onError={this.onError} />}
             {type === CONT_TYPE.YOUTUBE && <YouTube className={cx("cont-frame", type)}
-              style={{ width: "100%", height: "100%" }} url={props.url} playing={playing} controls={controls} playsinline loop />}
-            {/* width={width} height={height} url={props.url} playing controls />} */}
+              style={{ width: "100%", height: "100%" }} url={src} playing={playing} controls={controls} playsinline loop />}
             {type === CONT_TYPE.VIDEO && <ReactPlayer className={cx("cont-frame", type)}
-              style={{ width: "100%", height: "100%" }} url={props.url} playing={playing} controls={controls} playsinline loop />}
+              style={{ width: "100%", height: "100%" }} url={src} playing={playing} controls={controls} playsinline loop />}
             {type === CONT_TYPE.LINK && <img alt="link" className={cx("cont-frame", type, pointer)}
-              src={props.url} onLoad={this.onLoad} onError={this.onError} />}
-            {/* width={width} height={height} url={props.url} playing controls />} */}
-            {/* {type === CONT_TYPE.PDF && <img alt="img" className={cx("cont-frame", type, pointer, 'adobe')}
-              src={IMG.Pdf} url={props.link} onLoad={this.onLoad} onError={this.onError} />}
-            {type === CONT_TYPE.PDF && <span className={cx("cont-frame", 'pdf-view')}>{ST.PDF_VIEW}</span>} */}
+              src={src} onLoad={this.onLoad} onError={this.onError} />}
           </React.Fragment>
         )
       }
     }
 
-    const height = (type === CONT_TYPE.IMAGE) ? "fit-contents" : this.state.height;
+    const height = (type === CONT_TYPE.IMAGE) ? "fit-content" : this.state.height;
 
     return (
-      <StyledObject ref={ref => { this.box = ref }} className={cx("media-box", props.className)} style={props.style} onClick={this.onClickEdit} height={height} >
+      <StyledObject ref={ref => { this.box = ref }} className={cx("media-box", props.className)}
+        style={props.style} onClick={this.onClickEdit} height={height} border={border} >
         {renderContents()}
         {pointer && <Svg className="btn-down xxl box radius" onClick={this.onClicked} eid={EID.DOWM} name={"down"} color={'black'} />}
         {props.edited &&
---
+++ stories/28-Mediabox.stories.js
@@ -0,0 +1,182 @@
+import React, { useState } from 'react';
+import { optionsKnob as options, withKnobs, text, boolean, radios, number, button } from '@storybook/addon-knobs';
+import styled from 'styled-components';
+import cx from 'classnames/bind'
+import { Linebox } from './00-Frame';
+import { cs, Mediabox, Util } from '../src';
+import { IMG } from './sample/index';
+import { CONT_TYPE } from '../src/Mediabox';
+
+const StyledObject = styled.span`{
+  &.t-main {
+    .lb-box { ${cs.w.get(1024)} ${cs.p.b30} }
+
+    // .lb-box .lb-li > * { ${cs.m.b10} ${cs.h.get(400)} }
+    .lb-box .lb-li { ${cs.h.fit} ${({ height }) => height && cs.h.get(height)} }
+    .v-align .lb-li { ${cs.h.get(400)} } 
+
+    .res-view { 
+      ${cs.h.get(100)} ${cs.w.get(800)} ${cs.bg.lightgray} 
+      p { ${cs.m.a5} }
+    }    
+  }
+}`;
+
+export default { title: 'object|Mediabox', component: Mediabox, decorators: [withKnobs] };
+
+const samplecode = (value, classname = '') => `<Mediabox className={"${classname}"} ${value} />`;
+
+export const object = () => {
+  button('refresh(옵션을 변경 후 버튼을 클릭하세요)', () => onRefresh());
+  const size = options('size',
+    { 'none': '', 'full(100%)': 'full', 'normal(4:3)': 'normal', 'wide(16:9)': 'wide', 'xwide(21:9)': 'xwide', 'fwide(28:9)': 'fwide' },
+    '', { display: 'inline-radio' }, 'Other');
+  const width = text('width size', 'full');
+
+  const fit = options('object fit', { 'none': '', 'contain': 'contain', 'cover': 'cover', 'scale-down': 'scale-down', 'fill': 'fill' },
+    '', { display: 'inline-radio' }, 'Other');
+
+  const halign = options('horizantal align', { 'left': 'left', 'center': 'center', 'right': 'right' },
+    '', { display: 'inline-radio' }, 'Other');
+  const valign = options('vertical align', { 'top': 'top', 'middle': 'middle', 'bottom': 'bottom' },
+    '', { display: 'inline-radio' }, 'Other');
+
+
+  const isborder = boolean('border options', false);
+  const border = isborder ? text('border color', '#909090') : '';
+  const radius = isborder ? text('border radius', '5px') : '';
+  const borderwidth = isborder ? text('border width', '1px') : '';
+
+  const [result, setResult] = useState(null);
+  const [refresh, setRefresh] = useState(false);
+
+  const onClick = (eid, e) => {
+    setResult(`onChange(eid = ${eid}, e)`);
+  }
+
+  const onRefresh = (e) => {
+    setRefresh(true);
+    setTimeout(() => setRefresh(false), 200);
+  }
+
+  const opt = {
+    border: isborder ? { color: border, width: borderwidth, radius: radius } : null,
+  };
+
+  return (
+    <StyledObject className={"t-main"} width={width}>
+      <Linebox title={"toggle Mediabox"} className={"nomargin"}
+        desc={"Knobs 옵션을 통해 미리보기가 가능합니다."} sample={samplecode("", "")} box={true}>
+        {!refresh && <Mediabox className={cx(size, halign, valign, 'border')} fit={fit} options={opt} size={size}
+          type={CONT_TYPE.IMAGE} src={IMG.Image1} title={"scroll(float)"} onClick={onClick} eid={'Mediabox'} />}
+      </Linebox>
+
+      <div className={"res-view"}>
+        <p>onClick</p>
+        <p>{result}</p>
+      </div>
+    </StyledObject>
+  );
+};
+
+object.story = { name: 'Base' };
+
+export const size = () => {
+  return (
+    <StyledObject className={"t-main"}>
+      <Linebox title={"size normal(4:3)"} sample={samplecode("size={'normal'}", 'left')} >
+        <Mediabox src={null} className={'border'} size={'normal'} />
+      </Linebox>
+
+      <Linebox title={"size wide(16:9)"} sample={samplecode("size={'wide'}", 'left')} >
+        <Mediabox src={IMG.Image3} className={'border'} size={'wide'} />
+      </Linebox>
+
+      <Linebox title={"size xwide(21:9)"} sample={samplecode("size={'xwide'}", 'left')} >
+        <Mediabox src={IMG.Image1} className={'border'} size={'xwide'} />
+      </Linebox>
+
+      <Linebox title={"size fwide(28:9)"} sample={samplecode("size={'fwide'}", 'left')} >
+        <Mediabox src={IMG.Image4} className={'border'} size={'fwide'} />
+      </Linebox>
+
+      <Linebox title={"size full(w:100%)"} sample={samplecode("size={'full'}", 'left')} >
+        <Mediabox src={IMG.Image2} className={'border'} size={'full'} />
+      </Linebox>
+    </StyledObject>
+  );
+};
+
+export const box = () => {
+  return (
+    <StyledObject className={"t-main"}>
+      <Linebox title={"border"} sample={samplecode("size={'wide'}", 'border')} >
+        <Mediabox src={IMG.Image1} className={'border'} size={'wide'} />
+      </Linebox>
+
+      <Linebox title={"border radius"} sample={samplecode("size={'wide'}", 'border radius')} >
+        <Mediabox src={IMG.Image1} className={'border radius'} size={'wide'} />
+      </Linebox>
+    </StyledObject>
+  );
+};
+
+export const align = () => {
+  return (
+    <StyledObject className={"t-main"}>
+      <Linebox title={"horizontal align"} className={"align"} sample={samplecode()} box={true}>
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"left"} />
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"center"} />
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"right"} />
+      </Linebox>
+
+      <Linebox title={"vertical align"} className={"align v-align"} sample={samplecode()} box={true}>
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"top"} />
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"middle"} />
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"bottom"} />
+      </Linebox>
+
+      <Linebox title={"align"} className={"align v-align"} sample={samplecode()} box={true}>
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"left top"} />
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"center top"} />
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"right top"} />
+
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"left middle"} />
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"center middle"} />
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"right middle"} />
+
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"left bottom"} />
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"center bottom"} />
+        <Mediabox src={IMG.Image1} size={'wide'} maxheight={"100px"} className={"right bottom"} />
+      </Linebox>
+    </StyledObject>
+  );
+};
+
+export const border = () => {
+  return (
+    <StyledObject className={"t-main"} >
+      <Linebox title={"border options"} sample={samplecode('', 'sky')}>
+        <Mediabox className={"cover lg"} src={IMG.Image1} size={'wide'} options={{ border: { color: "red", width: '1px', radius: "5px" } }} />
+      </Linebox>
+
+      <Linebox title={"border options"} sample={samplecode('', 'sky')}>
+        <Mediabox className={"cover lg"} src={IMG.Image1} size={'wide'} options={{ border: { color: "blue", width: '2px', radius: "10px" } }} />
+      </Linebox>
+
+      <Linebox title={"border options"} sample={samplecode('', 'sky')}>
+        <Mediabox className={"cover lg"} src={IMG.Image1} size={'wide'} options={{ border: { color: "black", width: '5px', radius: "20px" } }} />
+      </Linebox>
+    </StyledObject>
+  );
+};
+
+export const edited = () => {
+  return (
+    <StyledObject className={"t-main"} >
+      <Linebox title={"border options"} sample={samplecode('', 'sky')}>
+        <Mediabox className={"border"} src={''} size={'wide'} edited={true}/>
+      </Linebox>
+    </StyledObject>
+  );
+};

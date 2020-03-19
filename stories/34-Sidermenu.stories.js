2a348ea45fadf2532a4e282784c1d6b435b22c40
sentron-cho <sentron@hanmail.net>
2020-03-19 16:11:14 +0900

    해야할 오브젝트 정리

---
+++ stories/34-Sidermenu.stories.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { optionsKnob as options, withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import styled from 'styled-components';
+import cx from 'classnames/bind'
+import { Linebox } from './00-Frame';
+import { cs, Sidemenu } from '../src';
+
+const StyledObject = styled.span`{
+  &.t-main {
+    .lb-box { ${cs.w.get(800)} ${cs.p.b30} }
+    .lb-box .lb-li { ${cs.min.height(100)} overflow: initial; }
+    .lb-box .lb-li > * { ${cs.m.right(100)} ${cs.m.b20} }
+    .res-view { 
+      ${cs.h.get(100)} ${cs.w.get(800)} ${cs.bg.lightgray} 
+      p { ${cs.m.a5} }
+    }
+  }
+}`;
+
+export default {
+  title: 'object|Sidemenu', // 스토리북에서 보여질 그룹과 경로를 명시
+  component: Sidemenu, // 어떤 컴포넌트를 문서화 할지 명시
+  decorators: [withKnobs], // 애드온 적용
+};
+
+const samplecode = (value, classname = '') => `<Sidemenu  className={"${classname}"} ${value} />`;
+export const sobject = () => {
+
+  const classname = text('classname', '');
+  const size = options('size',
+    { 'md(middle)': 'md', 'xl(xlarge)': 'xl', 'lg(large)': 'lg', 'sm(small)': 'sm', 'xs(xsmall)': 'xs' },
+    '', { display: 'inline-radio' }, 'Other');
+  const halign = options('horizontal', { 'left': 'left', 'center': 'center', 'right': 'right' },
+    '', { display: 'inline-radio' }, 'Other');
+  const valign = options('vertical', { 'top': 'top', 'middle': 'middle', 'bottom': 'bottom' },
+    '', { display: 'inline-radio' }, 'Other');
+  const bg = options('background',
+    { trans: 'trans', orange: 'orange', green: 'green', red: 'red', primary: 'primary', gray: 'gray', dark: 'dark', black: 'black' },
+    '', { display: 'inline-radio' }, 'Other');
+  const title = text('title', 'Sidemenu');
+
+  const max = number('max value', 100);
+  const min = number('min value', 0);
+  const current = number('current value', 50);
+  const limit = number('limit value(alert limit)', 80);
+
+  const [result, setResult] = useState(null);
+
+  const onClick = (eid, e) => {
+    setResult(`eid = ${eid}, e`);
+  }
+
+  return (
+    <StyledObject className={"t-main"}>
+      <Linebox title={"Sidemenu"} className={""} id={"f0001"} desc={"Knobs 옵션을 통해 미리보기가 가능합니다."} top={option.top}
+        sample={samplecode('title={"Sidemenu"} onClick={onClick} onChange={onChange}', 'primary')} box={true}>
+      </Linebox>
+
+      <div className={"res-view"}>
+        <p>onClick</p>
+        <p>{result}</p>
+      </div>
+    </StyledObject>
+  );
+};
+
+
+sobject.story = { name: 'Base' };

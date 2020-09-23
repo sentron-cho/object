73b727524cd3d37d246772a33a18b8fac03422a0
cho byungho <sentron@daum.net>
2020-09-23 15:54:33 +0900

    update

--- src/css-style.js
+++ src/css-style.js
@@ -157,7 +157,8 @@ const cs = {
     cover: 'object-fit: cover;',
     fill: 'object-fit: fill;',
     center: 'object-position: center center;',
-
+    
+    shadow: (v) => v ? v : `box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);`,
     fit: (v) => `object-fit: ${v};`,
     position: (v) => `object-position: ${v};`
   },

1299c6b0e78dfeb2a40dd4fb9eda65b3fce249e5
cho byungho <sentron@daum.net>
2020-12-18 08:22:15 +0900

    update

---
+++ src/Error.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import cs from './css-style';
+import cx from 'classnames/bind';
+
+const StyledObject = styled.div`{
+  &.err-page {
+
+    .ep-title { ${cs.align.center} ${cs.font.get(80)} ${cs.font.darkgray} ${cs.font.style('East Sea Dokdo')} 
+      ${cs.font.prewrap} ${cs.font.breakword}
+    }
+
+    &.t1 { .ep-title { ${cs.font.get(30)} } }
+    &.t2 { .ep-title { ${cs.font.get(40)} } }
+    &.t3 { .ep-title { ${cs.font.get(50)} } }
+    &.t4 { .ep-title { ${cs.font.get(80)} } }
+  
+    @media screen and (max-width : 860px) {
+    }
+}`;
+
+
+const Error = (props) => {
+  const {title = null, className = '' } = props;
+  // console.log(match.params.title);
+  return <StyledObject className={cx('err-page', className)}>
+    <p className={'ep-title'}>{title || '404 - PAGE NOT FOUND'}</p>
+  </StyledObject>
+}
+
+export default Error;

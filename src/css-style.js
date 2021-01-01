746de4ffe4f203fbcd9b9850019db519a2855c95
sentron <47094028+sentron-cho@users.noreply.github.com>
2021-01-01 22:28:28 +0900

    update

--- src/css-style.js
+++ src/css-style.js
@@ -360,7 +360,7 @@ const cs = {
     minus: `color: ${color.minus};`,
     plus: `color: ${color.plus};`,
     preline: 'white-space: pre-line; word-break: break-all;',
-    prewrap: 'white-space: pre-wrap; word-break: keep-all;',
+    prewrap: 'white-space: pre-wrap; word-break: break-all;',
     underline: 'text-decoration: underline;',
     cancelline: 'text-decoration: line-through;',
     noneline: 'text-decoration: none;',

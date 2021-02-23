2778ba1e0b9f7b85ace96182f7be3572a7df2ae9
cho byungho <sentron@daum.net>
2021-02-23 16:15:09 +0900

    update

--- src/Mediabox.js
+++ src/Mediabox.js
@@ -128,7 +128,11 @@ export default class Mediabox extends React.PureComponent {
 
   onClicked = (e) => {
     const { props } = this;
-    !Util.isEmpty(props.link) && window.open(props.link)
+    if (props.onClick) {
+      props.onClick('link');
+    } else {
+      !Util.isEmpty(props.link) && window.open(props.link)
+    }
   }
 
   getHeight = () => {
@@ -174,13 +178,13 @@ export default class Mediabox extends React.PureComponent {
       } else {
         return (
           <React.Fragment>
-            {type === CONT_TYPE.IMAGE && <img alt="img" className={cx("cont-frame", type, pointer)}
+            {type === CONT_TYPE.IMAGE && <img alt="img" className={cx("cont-frame", type, pointer)} onClick={this.onClicked}
               src={src} onLoad={this.onLoad} onError={this.onError} style={{ objectFit: fit, maxHeight, ...imagestyle }} />}
             {type === CONT_TYPE.YOUTUBE && <YouTube className={cx("cont-frame", type)} style={{ width: "100%", height: "100%", ...imagestyle }}
               url={src} playing={playing} controls={controls} playsinline loop />}
             {type === CONT_TYPE.VIDEO && <ReactPlayer className={cx("cont-frame", type)} style={{ width: "100%", height: "100%", ...imagestyle }}
               url={src} playing={playing} controls={controls} playsinline loop />}
-            {type === CONT_TYPE.LINK && <img alt="link" className={cx("cont-frame", type, pointer)}
+            {type === CONT_TYPE.LINK && <img alt="link" className={cx("cont-frame", type, pointer)} onClick={this.onClicked}
               src={src} onLoad={this.onLoad} onError={this.onError} style={{ ...imagestyle }} />}
           </React.Fragment>
         )
